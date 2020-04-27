0cb47e9b09b1afd6bbec9dfe14027d786df5cc11
Ashish <ashishnagar1221@gmail.com>
2020-04-27 18:46:32 +0530

    ...

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const Users = require('./models/User');
 
 
 const app = express();
-//const URI = "mongodb://127.0.0.1:27017/Blog"
 const URI = "mongodb+srv://user:root@taramandal-puhil.mongodb.net/Blog?retryWrites=true&w=majority"
 
 mongoose.connect(URI,  {
